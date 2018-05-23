b9ad3fbb98a32baaa744c162132447ce8e752cbc
Hello World! のTextViewを削除。 <syyyow110@gmail.com>
2018-05-23 18:17:16 +0900

    AisatsuApp修正

--- app/src/main/java/jp/techacademy/shoichi/ito/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/shoichi/ito/aisatsuapp/MainActivity.java
@@ -15,7 +15,6 @@ import android.widget.TimePicker;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mTextView;
-    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +22,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListner(this);
+        button1.setOnClickListener(this);
 
         mTextView = (TextView) findViewById(R.id.textView);
-        mEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            showTimePickerDialog();
+        }
     }
 
     private void showTimePickerDialog() {
@@ -35,11 +40,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        if (hourOfDay >= 2 && hourOfDay < 10) {
+                            mTextView.setText("おはよう");
+                        }else if (hourOfDay >= 10 && hourOfDay < 18) {
+                            mTextView.setText("こんにちは");
+                        }else if (hourOfDay >= 18 || hourOfDay < 2) {
+                            mTextView.setText("こんばんは");
+                        }
                     }
                 },
                 13, // 初期値（時間）
                 0,  // 初期値（分）
                 true);
+
         timePickerDialog.show();
     }
 }
